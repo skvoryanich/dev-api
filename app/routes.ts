900c386805b7ac6ee5e4882c9c328ed9fb331b91
skvoryanich <skvoryanich@gmail.com>
2022-06-22 21:52:58 +0300

    незначительные правки

--- app/routes.ts
+++ app/routes.ts
@@ -2,7 +2,7 @@ export function routes(app: any) {
     app.get('/', (req: any, res: any) => {
         res.json({
             type: 'message',
-            body: 'RESTfull api for telegram reminds';
+            body: 'RESTfull api for telegram reminds'
         });
     })
 
