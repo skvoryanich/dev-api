a45cfa03e248bfa9c3b6aba0d182be8f7d6046ec
skvoryanich <skvoryanich@gmail.com>
2022-06-22 21:51:26 +0300

    добавлен / маршрут

--- app/routes.ts
+++ app/routes.ts
@@ -1,4 +1,8 @@
 export function routes(app: any) {
+    app.get('/', (req: any, res: any) => {
+        res.send('RESTfull api for telegram reminds');
+    })
+
     app.get('/bots', (req: any, res: any) => {
         res.send('lalala');
     })
