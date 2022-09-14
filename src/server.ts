ffe20f490c267272d6c34eee89ab2e3ee0288290
skvoryanich <skvoryanich@gmail.com>
2022-09-14 19:22:17 +0300

    add express-handlebars

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import express, {Application, Request, Response} from "express";
 import {AuthRoutes} from "./routes/authRoutes";
+import { engine } from "express-handlebars";
+import path from "path";
 
 export class Server {
     private authRoutes: AuthRoutes;
@@ -14,13 +16,16 @@ export class Server {
 
     public configuration(){
         this.app.set('port', 8080);
+        this.app.use(express.static('public'));
+        this.app.engine('handlebars', engine());
+        this.app.set('view engine', 'handlebars');
+        this.app.set("views", path.join(__dirname, "views"));
     }
 
     public routes(){
         this.app.get("/", (req: Request, res:Response) => {
-            res.json({
-                type: 'message',
-                message: 'Главная страница. Тут должен быть редирект на авторизацию, как и на всех страницах - если нет куки с именем JTOKEN или он равняется empty'
+            res.render('home', {
+                text: 'Главная страница. Тут должен быть редирект на авторизацию, как и на всех страницах - если нет куки с именем JTOKEN или он равняется empty'
             });
         });
         this.app.use(`/auth/`, this.authRoutes.router);
